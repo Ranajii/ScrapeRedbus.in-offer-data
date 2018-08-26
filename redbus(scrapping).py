5aa08c717dcc9709e74312b85a1f2a3fe3554603
Vishal Kumar Rana <42720439+Ranajii@users.noreply.github.com>
2018-08-27 00:17:24 +0530

    Update redbus(scrapping).py
    Working on Python basics for a few days, been playing with this library a few hours ago. Made this script to scrape data from redbus.in offers page. 
    I do understand I made it in a very childish manner, like assigning variables at multiple positions. All I did is to make debugging easier for me. 
    Happy to receive any feedback. Thank you.

--- redbus(scrapping).py
+++ redbus(scrapping).py
@@ -3,30 +3,21 @@
 
 # REDBUS #
 
+#importing basic libraries
 from selenium import webdriver
 import time
 import requests
 from bs4 import BeautifulSoup
-driver = webdriver.Chrome(r"C:\Users\visha\chromedriver")
+driver = webdriver.Chrome(r"C:\Users\visha\chromedriver") #path of your chromedriver
 
 
 url = 'https://www.redbus.in/info/OfferTerms'
 page = requests.get(url)
 soup = BeautifulSoup(page.content,'html.parser')
 
-# Using Chrome to access web
-
-
-#driver.get(url)
-#driver.find_element_by_xpath('''//*[@id="mBWrapper"]/table/tbody/tr[1]/td[1]''').click()
-#driver.back()
-#
-#driver.forward()
-#driver.find_element_by_xpath('''//*[@id="mBWrapper"]/table/tbody/tr[1]/td[2]''').click()
-
 driver.get(url)
 termsbox = '//*[@id="mBWrapper"]/table/tbody/tr[1]/td[1]'
-collect = []
+collect = []  #here all the data is collected
 for j in range(1,6):
     for i in range(1,4):
         offernum = str('Offer ')+str(j)+'-'+str(i)
@@ -41,24 +32,3 @@ for j in range(1,6):
         driver.forward()
         collect.extend([[offernum,offername,linknew]])
         
-
-############################
-'for 1 link'
-from selenium import webdriver
-import time
-import requests
-from bs4 import BeautifulSoup
-driver = webdriver.Chrome(r"C:\Users\visha\chromedriver")
-
-
-url = 'https://www.redbus.in/info/OfferTerms'
-page = requests.get(url)
-soup = BeautifulSoup(page.content,'html.parser')
-driver.get(url)
-termsbox = '//*[@id="mBWrapper"]/table/tbody/tr[1]/td[1]'
-driver.find_element_by_xpath(termsbox).click()
-name = driver.find_element_by_id('OfferDiscription').text
-
-
-
-
